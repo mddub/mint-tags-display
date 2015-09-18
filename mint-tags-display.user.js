691b6507cbbd6483c31c66b4cd2f2266be8aa054
Mark Wilson <mark@warkmilson.com>
2015-09-17 17:48:13 -0700

    v1.0.0

--- mint-tags-display.user.js
+++ mint-tags-display.user.js
@@ -4,7 +4,7 @@
 // @description Show tags in the transactions listing on Mint.com.
 // @namespace   com.warkmilson.mint.js
 // @author      Mark Wilson
-// @version     0.1.0
+// @version     1.0.0
 // @homepage    https://github.com/mddub/mint-tags-display
 // @updateURL   https://github.com/mddub/mint-tags-display/raw/master/mint-tags-display.user.js
 // @downloadURL https://github.com/mddub/mint-tags-display/raw/master/mint-tags-display.user.js
